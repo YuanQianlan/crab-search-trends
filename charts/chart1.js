2130234cca84da30e33c809596044d2b31b7806f
YuanQianlan <3084858217@qq.com>
2026-07-31 17:43:01 +0800

    Rename province search trend chart

--- charts/chart1.js
+++ charts/chart1.js
@@ -1,6 +1,6 @@
 window.CHART_PAGE_CONFIG = {
   pageId: "chart1",
-  documentTitle: "各省份搜索总量年度变化",
-  chartTitle: "2011—2026年各省份搜索总量变化",
+  documentTitle: "2011~2026年各省份河蟹百度指数搜索总量",
+  chartTitle: "2011~2026年各省份河蟹百度指数搜索总量",
   // 如需换成其他图表，可在这里增加：option: { ...完整 ECharts 配置... }
 };
